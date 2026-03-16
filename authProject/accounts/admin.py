42e58cc8ea85aaf7ffb3f45fbf85dc1508366203
SaketJ3003 <saketjangir3003@gmail.com>
2026-03-16 18:06:17 +0530

    Made some incremental changes.

--- authProject/accounts/admin.py
+++ authProject/accounts/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import UserInfo, TempOtp, TempUser, State,UserToken
+from .models import UserInfo, TempUser, State, UserToken, EmailVerificationToken, OTP
+
 # Register your models here.
 admin.site.register(UserInfo)
-admin.site.register(TempOtp)
 admin.site.register(TempUser)
 admin.site.register(State)
 admin.site.register(UserToken)
+admin.site.register(OTP)
+admin.site.register(EmailVerificationToken)
